1751e6b6b184b749c37001b8a7a1475f33c62593
lanirudhreddy <152101059+lanirudhreddy@users.noreply.github.com>
2026-03-12 21:10:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    fullscreen: true,
+    resizable: true
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
